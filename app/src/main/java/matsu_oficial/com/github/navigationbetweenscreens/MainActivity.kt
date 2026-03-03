8963618b716b3b4362241aa4c8b69056f010ba1f
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-03 20:48:47 -0300

    Projeto Kotlin inicial

---
+++ app/src/main/java/matsu_oficial/com/github/navigationbetweenscreens/MainActivity.kt
@@ -0,0 +1,44 @@
+package matsu_oficial.com.github.navigationbetweenscreens
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import matsu_oficial.com.github.navigationbetweenscreens.ui.theme.NavigationbetweenscreensTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavigationbetweenscreensTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NavigationbetweenscreensTheme {
+        Greeting("Android")
+    }
+}
